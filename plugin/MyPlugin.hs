56ce0420d754411e5a3b00b3816061cf2bbf0dfe
Patrick <fwy996602672@gmail.com>
2024-07-17 04:40:26 +0800

    add Ext

--- plugin/MyPlugin.hs
+++ plugin/MyPlugin.hs
@@ -3,11 +3,17 @@
 module MyPlugin (plugin) where
 
 import GHC.Plugins
+import GHC.Tc.Types
+import GHC.Hs
+import Ext.Ast
+import Ext.Types
 
 -- This is the entry point for the plugin
 plugin :: Plugin
 plugin = defaultPlugin {
-    installCoreToDos = install
+    installCoreToDos = install,
+    typeCheckResultAction = typechecked,
+    renamedResultAction = renamedResult
 }
 
 -- This function modifies the compilation pipeline
@@ -22,3 +28,19 @@ pass guts = do
     -- Do something with the Core here
     putMsgS "Running my pass"
     return guts
+
+renamedResult :: [CommandLineOption] -> TcGblEnv -> HsGroup GhcRn -> TcM (TcGblEnv, HsGroup GhcRn)
+renamedResult _ env hsGroup = do
+    return (env{tcg_rn_decls=Just hsGroup}, hsGroup)
+
+
+typechecked :: [CommandLineOption] -> ModSummary -> TcGblEnv -> TcM TcGblEnv
+typechecked _ ms env = do
+    let (Just renamed) = tcg_rn_decls env
+        ex = tcg_rn_imports env
+        ix = tcg_rn_exports env
+        (lhrn, xrec) = tcg_hdr_info env
+    hf <- mkHieFile ms env (renamed, ex, ix, lhrn, xrec)
+    liftIO $ putStrLn $ showSDocUnsafe $ ppr (renamed, ex, ix, lhrn, xrec)
+    liftIO $ putStrLn $ showSDocUnsafe $ ppr (hie_asts hf)
+    return env
