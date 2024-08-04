3c96ae1ae52f16f4ac1978582c3eb7418e055565
Patrick <fwy996602672@gmail.com>
2024-08-04 14:33:44 +0800

    cleanup

--- plugin/MyPlugin.hs
+++ plugin/MyPlugin.hs
@@ -149,7 +149,7 @@ typechecked _ ms env = do
     let docs = hieFileScip dy kindMap unit_state hf
     forM_ docs $ \doc -> do
         let encoded = encodeMessage doc
-            path = "data" </> T.unpack (doc ^. Proto.Scip_Fields.relativePath)
+            path = "./data" </> T.unpack (doc ^. Proto.Scip_Fields.relativePath)
         -- -- create a file
         liftIO $ BS.createDirectoryIfMissing True $ takeDirectory path
         liftIO $ BS.writeFile path encoded
